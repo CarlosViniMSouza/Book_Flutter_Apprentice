621c4cce35bda6965a607f2dc3da054d26ccb028
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-10-25 10:56:00 -0300

    main.dart in restauration process

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'recipe.dart';
 
 void main() {
   runApp(const RecipeApp());
 }
 
 class RecipeApp extends StatelessWidget {
+
   const RecipeApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
@@ -42,11 +44,94 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SafeArea(
         // TODO: Replace child: Container()
         // 4
-        child: Container(),
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            // TODO: Update to return Recipe card
+            // 7
+            return GestureDetector(
+              // 8
+              onTap: () {
+                // 9
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      // 10
+                      // TODO: Replace return with return RecipeDetail()
+                      return const Text('Detail page');
+                    },
+                  ),
+                );
+              },
+              // 11
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+
+          },
+        ),
       ),
     );
   }
 
 // TODO: Add buildRecipeCard() here
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      // 1
+      elevation: 2.0,
+      // 2
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)),
+      // 3
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // 4
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            // 5
+            const SizedBox(
+              height: 14.0,
+            ),
+            // 6
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
 
 }
+
+// 1
+@override
+Widget build(BuildContext context) {
+  // 2
+  final ThemeData theme = ThemeData();
+  // 3
+  return MaterialApp(
+    // 4
+    title: 'Recipe Calculator',
+    // 5
+    theme: theme.copyWith(
+      colorScheme: theme.colorScheme.copyWith(
+        primary: Colors.grey,
+        secondary: Colors.black,
+      ),
+    ),
+    // 6
+    home: const MyHomePage(title: 'Recipe Calculator'),
+  );
+}
+
