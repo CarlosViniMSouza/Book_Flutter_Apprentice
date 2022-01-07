f84385cd82dda14e07d50ea19db70e6d517e6dc1
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-07 16:50:19 -0300

    assets: img -> add | Project Restarted

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "assets: img -> add | Project Restarted"
+git commit -m "assets: img -> add | Project Restarted | Restart connect GitHub"
 git push origin HEAD
 git status
 clear
