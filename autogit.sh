3f53c94a3ca541ae372aa6471718fbc71170cf69
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-28 14:38:13 -0300

    folders android & ios -> removed | Testing project in WebStorm!

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "LessonsBook folder -> removed | card1.dart -> add"
+git commit -m "folders android & ios -> removed | Testing project in WebStorm!"
 git push origin HEAD
 git status
 clear
